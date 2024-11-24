1911860d9495ed6295d0e913fdc40d31187effec
yangzhengpeng01 <yangzhengpeng01@bilibili.com>
2024-11-24 14:59:39 +0800

    项目初始化

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import App from './App.vue'
+import router from './router'
+import Aura from '@primevue/themes/aura'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <div class="home-view">
+    <Button>hello</Button>
+  </div>
+</template>
